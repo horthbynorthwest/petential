b6691da36454892ced2e6b53fa6bad777c8a459d
Melissa <mystic101@gmail.com>
2021-01-20 17:37:00 +0000

    food api/serializers and partial food api/views

--- pet_tential/api/serializers.py
+++ pet_tential/api/serializers.py
@@ -5,3 +5,8 @@ class FoodSerializer(serializers.ModelSerializer):
     class Meta:
         model = Food
         fields = ('id', 'meal_type', 'date', 'fed_at', 'comment', 'treats')
+
+class CreateFoodSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Food
+        fields = ('meal_type', 'date', 'comment', 'treats')
--- pet_tential/api/views.py
+++ pet_tential/api/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-from rest_framework import generics
-from .serializers import FoodSerializer
+from rest_framework import generics, status
+from .serializers import FoodSerializer, CreateFoodSerializer
 from .models import Food
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
-class FoodView(generics.CreateAPIView):
+class FoodView(generics.ListAPIView):
     queryset = Food.objects.all()
     serializer_class = FoodSerializer
+
+class CreateFoodView(APIView):
+    
