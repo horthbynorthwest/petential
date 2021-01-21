e5476a8efb75ea31eae40efb59b17dd370b6d6f8
Melissa <mystic101@gmail.com>
2021-01-21 14:18:44 +0000

    backend for food list and food create

--- pet_tential/api/views.py
+++ pet_tential/api/views.py
@@ -11,4 +11,18 @@ class FoodView(generics.ListAPIView):
     serializer_class = FoodSerializer
 
 class CreateFoodView(APIView):
-    
+    serializer_class = CreateFoodSerializer
+
+    def post(self, request, format=None):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            meal_type = serializer.data.get('meal_type')
+            date = serializer.data.get('date')
+            comment = serializer.data.get('comment')
+            treats = serializer.data.get('treats')
+            
+            food = Food(meal_type=meal_type, date=date, comment=comment, treats=treats)
+            food.save()
+            return Response(FoodSerializer(food).data, status=status.HTTP_201_CREATED)
+        
+        return Response({'Bad Request': 'Invalid data...'}, status=status.HTTP_400_BAD_REQUEST)
