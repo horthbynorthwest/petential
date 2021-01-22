99f8f93087920544dedc444dfdb2bc530f359718
savannaelbey <savanna_e@icloud.com>
2021-01-22 17:08:11 +0000

    install jest - 1st test failing

--- pet_tential/frontend/src/tests/FoodPage.test.js
+++ pet_tential/frontend/src/tests/FoodPage.test.js
@@ -1,27 +1,26 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
-import FoodPage from '../FoodPage';
+import ReactDOM from 'react-dom';
+import TestRenderer, { act } from 'react-test-renderer';
+//import renderer from 'react-test-renderer';
+import FoodPage from '../components/FoodPage';
+import {cleanup, fireEvent, render} from '@testing-library/react';
 
 // feature - go to page food and expect content to be 'Food Log'
 // feature - fill in form and click Post, expect page to have content that they've just added
-// unit - 
-
-test('Link changes the class when hovered', () => {
-  const component = renderer.create(
-    <Link page="http://www.facebook.com">Facebook</Link>,
-  );
-  let tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
-
-  // manually trigger the callback
-  tree.props.onMouseEnter();
-  // re-rendering
-  tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
-
-  // manually trigger the callback
-  tree.props.onMouseLeave();
-  // re-rendering
-  tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
+// unit -
+describe ('Test the FoodPage and actions', () => {
+  test('handleMealTypeChange method changes the mealType state', () => {
+    let foodpage = new FoodPage();
+    const handleMealTypeChange = jest.fn();
+    foodpage.handleMealTypeChange('lunch');
+    expect(foodpage.mealType).toEqual('lunch');
+  });
 });
+
+
+// it('should return the initial state', () => {
+//     let foodpage = new FoodPage();
+//     expect(foodpage.initialState).toContain({ mealType: "",
+//     comment: "",
+//     treats: 0, })
+// })
