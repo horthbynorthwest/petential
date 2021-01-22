bf57743399f26c77295eb2cb92c3943a1c2c4a92
Georgie Horth <georgiehorth@gmail.com>
2021-01-22 13:04:33 +0000

    fixed server issues

--- pet_tential/api/views.py
+++ pet_tential/api/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
-from rest_framework import generics
-from .serializers import FoodSerializer
+from rest_framework import generics, status
+from .serializers import FoodSerializer, CreateFoodSerializer
 from .models import Food
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
-class FoodView(generics.CreateAPIView):
+class FoodView(generics.ListAPIView):
     queryset = Food.objects.all()
     serializer_class = FoodSerializer
+
+class CreateFoodView(APIView):
+    serializer_class = CreateFoodSerializer
+
+    def post(self, request, format=None):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            meal_type = serializer.data.get('meal_type')
+            date = serializer.data.get('date')
+            comment = serializer.data.get('comment')
+            treats = serializer.data.get('treats')
+            
+            food = Food(meal_type=meal_type, date=date, comment=comment, treats=treats)
+            food.save()
+            return Response(FoodSerializer(food).data, status=status.HTTP_201_CREATED)
+        
+        return Response({'Bad Request': 'Invalid data...'}, status=status.HTTP_400_BAD_REQUEST)
--- pet_tential/frontend/src/components/FoodPage.js
+++ pet_tential/frontend/src/components/FoodPage.js
@@ -1,13 +1,161 @@
 import React, { Component } from "react";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    Redirect,
+  } from "react-router-dom";
+  import Button from "@material-ui/core/Button";
+  import Grid from "@material-ui/core/Grid";
+  import Typography from "@material-ui/core/Typography";
+  import TextField from "@material-ui/core/TextField";
+  import Container from "@material-ui/core/Container";
+  import InputLabel from "@material-ui/core/InputLabel";
+  import FormHelperText from "@material-ui/core/FormHelperText";
+  import FormControl from "@material-ui/core/FormControl";
+  import Radio from "@material-ui/core/Radio";
+  import RadioGroup from "@material-ui/core/RadioGroup";
+  import FormControlLabel from "@material-ui/core/FormControlLabel";
+  import { makeStyles } from '@material-ui/core/styles';
+
 
 export default class FoodPage extends Component {
-  constructor(props) {
-    super(props);
-  }
+    defaultTreats = 0
+    
+    constructor(props) {
+        super(props);
 
-  render() {
-    return (
-        <p>This is the food page</p>
-    );
-  }
+        var today = new Date(),
+        defaultDate = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+
+        this.state = {
+            mealType: "",
+            date: this.defaultDate,
+            comment: "",
+            treats: this.defaultTreats,
+          };
+
+        this.handleMealTypeChange = this.handleMealTypeChange.bind(this);
+        this.handleDateChange = this.handleDateChange.bind(this);
+        this.handleCommentChange = this.handleCommentChange.bind(this);
+        this.handleTreatsChange = this.handleTreatsChange.bind(this);
+        this.handleSubmitButtonPressed = this.handleSubmitButtonPressed.bind(this);
+    }
+
+    handleMealTypeChange(e) {
+        this.setState({
+          mealType: e.target.value,
+        });
+    }
+
+    handleDateChange(e) {
+        this.setState({
+          date: e.target.value,
+        });
+    }
+
+    handleCommentChange(e) {
+        this.setState({
+          comment: e.target.value,
+        });
+    }
+
+    handleTreatsChange(e) {
+        this.setState({
+          treats: e.target.value,
+        });
+    }
+
+    handleSubmitButtonPressed() {
+        console.log("at the top");
+        const requestOptions = {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+              meal_type: this.state.mealType,
+              date: this.state.date,
+              comment: this.state.comment,
+              treats: this.state.treats,
+            }),
+          };
+          console.log("hi");
+
+          fetch("/api/add-food", requestOptions)
+            .then((response) => response.json())
+            .then((data) => console.log(data));
+
+        
+    }
+
+    render() {
+        return (
+            <form>
+              <Grid container spacing={3}>
+                <Grid item xs={12} align="center">
+                    <Typography component="h4" variant="h4">
+                        Food Log
+                    </Typography>
+                </Grid>
+                <Grid item xs={12}>
+                  <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                      <TextField 
+                      fullWidth 
+                      label="Meal Type" 
+                      name="MealType" 
+                      size="small" 
+                      variant="outlined" 
+                      onChange={this.handleMealTypeChange} 
+                      />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <TextField 
+                      fullWidth 
+                      type="date" 
+                      size="small" 
+                      variant="outlined" 
+                      onChange={this.handleDateChange} 
+                      defaultValue={this.defaultDate} 
+                      />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <TextField 
+                      fullWidth 
+                      label="Comment" 
+                      name="Comment" 
+                      size="small" 
+                      variant="outlined" 
+                      onChange={this.handleCommentChange} 
+                      />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <TextField 
+                      fullWidth 
+                      type="number" 
+                      label="Treats" 
+                      name="Treats" 
+                      size="small" 
+                      variant="outlined" 
+                      onChange={this.handleTreatsChange} 
+                      defaultValue={this.defaultTreats} 
+                      />
+                    </Grid>
+                  </Grid>
+                </Grid>
+                <Grid item xs={12}>
+                  <Button 
+                  color="primary" 
+                  fullWidth 
+                  type="submit" 
+                  variant="contained" 
+                  onClick={this.handleSubmitButtonPressed}
+                  >
+                    Post
+                  </Button>
+                </Grid>
+              </Grid>
+            </form>
+        );
+    }
 }
