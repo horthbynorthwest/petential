7705f13a5eea969a9a4cbc1ca033f8a08a3bac7a
Georgie Horth <georgiehorth@gmail.com>
2021-01-27 16:02:07 +0000

    Changed some styling around the weather API

--- pet_tential/frontend/src/components/Weather.js
+++ pet_tential/frontend/src/components/Weather.js
@@ -20,45 +20,46 @@ function Weather() {
     }
   }
 
-  const dateBuilder = (d) => {
-    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  // const dateBuilder = (d) => {
+  //   let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  //   let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-    let day = days[d.getDay()];
-    let date = d.getDate();
-    let month = months[d.getMonth()];
-    let year = d.getFullYear();
+  //   let day = days[d.getDay()];
+  //   let date = d.getDate();
+  //   let month = months[d.getMonth()];
+  //   let year = d.getFullYear();
 
-    return `${day} ${date} ${month} ${year}`
-  }
+  //   return `${day} ${date} ${month} ${year}`
+  // }
 
   return (
     <div>
       <main>
+        <h5>Get today's weather to plan your walk!</h5>
+        <br />
         <div className="search-box">
           <input 
             type="text"
             className="search-bar"
-            placeholder="Search..."
+            placeholder="Enter Your City!"
             onChange={e => setQuery(e.target.value)}
             value={query}
             onKeyPress={search}
           />
         </div>
+        <br />
         {(typeof weather.main != "undefined") ? (
         <div>
-          <div className="location-box">
-            <div className="location">{weather.name}, {weather.sys.country}</div>
-            <div className="date">{dateBuilder(new Date())}</div>
-          </div>
-          <div className="weather-box">
-            <div className="temp">
-              {Math.round(weather.main.temp)}°c
-            </div>
-            <div className="weather">{weather.weather[0].main}</div>
-          </div>
+          <h5>
+              Today's temp in {weather.name}, {weather.sys.country} is {Math.round(weather.main.temp)}°c
+                with {weather.weather[0].description}
+          </h5>
         </div>
         ) : ('')}
+        <br />
+        <br />
+        <br />
+        <br />
       </main>
     </div>
   );
